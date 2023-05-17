decc529344cdc567c1ed2021766263d9fdc74669
Taeyooooon <eric568@naver.com>
2023-05-17 17:56:21 +0900

    Style: tailwind custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,20 @@ module.exports = {
   ],
   important: 'body',
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        1: '#2B2836',
+        support2: '#3A3649',
+        support3: '#514C66',
+        accent: '#2E2C3A',
+        trans: '#2A2735B2',
+      },
+      textColor: {
+        primary: '#B79494',
+        1: '#B6BFC8',
+        2: '#FFFFFF',
+      },
+    },
   },
   plugins: [],
   corePlugins: {
